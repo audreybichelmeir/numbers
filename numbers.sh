70220e1c3bb44a6bb3100ee0bcf0909acaf3910f
Audrey Bichelmeir <bichelmeir@chapman.edu>
2018-11-16 12:03:04 -0800

    adds numbers.sh

---
+++ numbers.sh
@@ -0,0 +1,21 @@
+#! /bin/bash
+# numbers.sh
+# Audrey Bichelmeir
+
+echo "Enter a positive number: "
+read number
+n=0
+while [ $n -lt $number ]
+do
+	n=$[$n+1]
+	if [ $(($n%2)) -eq 0 ]
+	then
+		echo "$n  Even"
+	else
+		echo "$n  Odd"
+	fi
+done
+
+
+
+
